afc4da3088587c91024b43499913a9af68d6d538
denshade <lveeckha@PC-17497.ai.internal.uzgent.be>
2016-01-01 22:34:22 +0100

    add commonwords to resources.

--- src/thelaboflieven/lidf/CommonWords.java
+++ src/thelaboflieven/lidf/CommonWords.java
@@ -3,8 +3,10 @@ package thelaboflieven.lidf;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -17,7 +19,7 @@ public class CommonWords
 
     private static void init() throws IOException {
         commonWords = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-        BufferedReader reader = new BufferedReader(new FileReader("commonwords.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("c:\\cache\\commonwords.txt"));
         String line = reader.readLine();
         while (line != null) {
             commonWords.add(line);
@@ -30,7 +32,6 @@ public class CommonWords
             init();
         if (!StringUtils.isAlphanumeric(word))
             return true;
-
 
         if (commonWords.contains(word))
             return true;
